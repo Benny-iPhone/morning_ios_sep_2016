6c85f5e025dbc84b2bc9b5c3e96bf38047088db3
Benny Davidovitz <benny@Bennys-MacBook-Air.local>
2016-11-07 15:32:05 +0200

    done with form project

--- Form/Form/AppTextField.swift
+++ Form/Form/AppTextField.swift
@@ -9,15 +9,59 @@
 import UIKit
 
 class AppTextField: UITextField {
+    
+    var showWarning : Bool{
+        set /*(newValue)*/{
+            self.rightViewMode = newValue ? .always : .never
+        }
+        get{
+            return rightViewMode == .always
+        }
+    }
+    
 
+    fileprivate func addToolBar(){
+        
+        let screenWidth = UIScreen.main.bounds.width
+        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 44)) //44 is recommended height for UIToolBar
+        
+        let title = self.returnKeyType == .done ? "Done" : "Next"
+        
+        
+        let nextButton = UIBarButtonItem(title: title, style: .plain, target: self, action: #selector(AppTextField.nextAction))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        toolBar.items = [spaceButton,nextButton]
+        
+        self.inputAccessoryView = toolBar
+    }
+    
+    func nextAction(){
+        sendActions(for: .editingDidEndOnExit)
+    }
+    
     fileprivate func initialize(){
-        font = UIFont(name: "Georgia", size: 16)
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "bug"))
+        self.rightView = imageView
+        self.showWarning = false
+
+        
+        //font = UIFont(name: "Georgia", size: 16)
         textColor = .purple
         
         clearsOnBeginEditing = false
         clearButtonMode = .whileEditing
         spellCheckingType = .no
         autocorrectionType = .no
+        
+        switch self.keyboardType {
+        case .phonePad, .numberPad:
+            addToolBar()
+        default:
+            return
+        }
+        
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -41,8 +85,92 @@ class PasswordTextField : AppTextField{
     }
 }
 
+class BirthdateTextField : AppTextField{
+    
+    var date : Date?
+    
+    fileprivate override func initialize() {
+        super.initialize()
+        
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        datePicker.backgroundColor = .white
+        datePicker.maximumDate = Date()
+        datePicker.date = Calendar.current.date(byAdding: .year, value: -18, to: Date()) ?? Date()
+        
+        datePicker.addTarget(self, action: #selector(BirthdateTextField.datePickerAction(_:)), for: .valueChanged)
+        
+        self.inputView = datePicker
+        
+        addToolBar()
+    }
+    
+    func datePickerAction(_ sender : UIDatePicker){
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        //dateFormatter.dateStyle = .medium
+        //dateFormatter.timeStyle = .none
+        self.text = dateFormatter.string(from: sender.date)
+        
+        self.date = sender.date
+        
+    }
+    
+    
+}
 
-
+class GenderTextField : AppTextField, UIPickerViewDataSource, UIPickerViewDelegate{
+    
+    var gender : Gender?
+    
+    enum Gender : Int {
+        case male
+        case female
+        
+        static let count = 2
+        
+        func title() -> String{
+            switch self {
+            case .female:
+                return "Female"
+            case .male:
+                return "Male"
+            }
+        }
+    }
+    
+    fileprivate override func initialize() {
+        super.initialize()
+        
+        addToolBar()
+        
+        let pickerView = UIPickerView()
+        pickerView.backgroundColor = .white
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        self.inputView = pickerView
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return Gender.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return Gender(rawValue: row)?.title()
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let g = Gender(rawValue: row)
+        self.text = g?.title()
+        self.gender = g
+    }
+}
 
 
 
--- Form/Form/ViewController.swift
+++ Form/Form/ViewController.swift
@@ -10,13 +10,18 @@ import UIKit
 
 class ViewController: UIViewController{
 
+    var topLayoutInitiateY : CGFloat!
+    
+    @IBOutlet weak var stackViewTopLayout: NSLayoutConstraint!
     @IBOutlet weak var lastNameTextField: AppTextField!
     @IBOutlet weak var firstNameTextField: AppTextField!
     
+    @IBOutlet weak var emailTextField: AppTextField!
     
     @IBAction func tapAction(_ sender: Any) {
         //firstNameTextField.resignFirstResponder()
         _ = self.view.hideKeyboard()
+        changeTopBy(0)
     }
     
     @IBAction func textFieldDidEndOnExitAction(_ sender: UITextField) {
@@ -31,32 +36,81 @@ class ViewController: UIViewController{
         
     }
     
+    @IBAction func textFieldChangedAction(_ sender : AppTextField){
+        sender.showWarning = false
+    }
+    
     @IBAction func doneAction(_ sender: Any) {
         tapAction(sender)
+        
+        
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //read default value from storyboard
+        topLayoutInitiateY = stackViewTopLayout.constant
+        
         firstNameTextField.becomeFirstResponder()
-        lastNameTextField.delegate = self
+        //lastNameTextField.delegate = self
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
 
 //MARK: - UITextFieldDelegate -
 extension ViewController : UITextFieldDelegate{
+    
+    func changeTopBy(_ y : CGFloat){
+        //dont do anything if nothing changed
+        guard stackViewTopLayout.constant != topLayoutInitiateY - y else {
+            return
+        }
+        
+        stackViewTopLayout.constant = topLayoutInitiateY - y
+       
+        func animateFunc(){
+            //refresh constraints
+            self.view.layoutSubviews()
+        }
+        
+        UIView.animate(withDuration: 0.8, delay: 0, options: .curveLinear, animations: animateFunc, completion: nil)
+        //UIView.animate(withDuration: 0.7, animations: animateFunc)
+    }
+    
     func textFieldDidBeginEditing(_ textField: UITextField) {
+        let y = textField.frame.origin.y - firstNameTextField.frame.origin.y
+
+        changeTopBy(y)
         
     }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        guard let textField = textField as? AppTextField else{
+            return true
+        }
+        
+        let text = textField.text ?? ""
+        
+        switch textField {
+        case firstNameTextField:
+            textField.showWarning = text.isEmpty
+        case emailTextField:
+            textField.showWarning = !text.isValidEmail()
+        default:
+            break
+        }
+        
+        
+        
+        return !textField.showWarning
+
+    }
+    
+    
     
     /*
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
